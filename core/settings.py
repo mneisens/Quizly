c965f59a28a39fc6973378e0283c973aca835829
michael <contact@michael-neisens.de>
2025-09-07 10:13:25 +0200

    add port 5500 in settings

--- core/settings.py
+++ core/settings.py
@@ -175,6 +175,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5000",
     "http://localhost:5001",
     "http://127.0.0.1:5001",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
     "http://localhost:5501",
     "http://127.0.0.1:5501",
     "http://localhost:5173",  
